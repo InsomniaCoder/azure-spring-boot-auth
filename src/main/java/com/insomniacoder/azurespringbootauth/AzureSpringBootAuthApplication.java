f0373ac32689b817dd71bb8c38f891c488c15169
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2018-10-08 17:49:37 +0700

    initial commit

---
+++ src/main/java/com/insomniacoder/azurespringbootauth/AzureSpringBootAuthApplication.java
@@ -0,0 +1,12 @@
+package com.insomniacoder.azurespringbootauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringBootAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringBootAuthApplication.class, args);
+	}
+}
